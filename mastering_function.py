d5312a4b5ec31b5b81d3e46128d7fdd0616a32fc
brucy34 <brucygio123@gmail.com>
2022-12-07 19:10:10 -0500

    corrections and begin exercice 5

--- mastering_function.py
+++ mastering_function.py
@@ -1,21 +1,33 @@
 import random
-# kod  aleyatwa alfabetik san repetisyon
+# 3.-kod  aleyatwa alfabetik san repetisyon
 alfa1="abcdefghijklmnopqrstuvwxyz"
 
-def aleyatwa(alfa,nomb):
+def aleyatwa1(alfa,nomb):
     return "".join(random.sample(alfa,nomb))
 
 
-kod1=aleyatwa(alfa1,10)
+kod1=aleyatwa1(alfa1,10)
 print(kod1)
 print(len(kod1))
 
 
-#  kod aleyatwa alfanimerik san repetisyon
+# 4.-kod aleyatwa alfanimerik san repetisyon
 
 alfa2="abcdefghijklmnopqrstuvwxyz1234567890"
 
-def aleyatwa(alfa,nomb):
+def aleyatwa2(alfa,nomb):
+    return "".join(random.sample(alfa,nomb))
+
+
+kod2=aleyatwa2(alfa2,36)
+print(kod2)
+print(len(kod2))
+
+# 5.-jenere yon SLUG 
+
+alfa3="yon chenn pou fe slug"
+
+def aleyatwa(alfa):
     return "".join(random.sample(alfa,nomb))
 
 
